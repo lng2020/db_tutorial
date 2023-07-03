7ae6424326b7182395a859f284bfd61a7dc50345
lng2020 <nanguanlin6@gmail.com>
2023-07-04 02:13:34 +0800

    binary search and duplicate keys

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -102,6 +102,7 @@ describe 'database' do
       "db > ",
     ])
   end 
+
   it 'prints constants' do
     script = [
       ".constants",
@@ -119,6 +120,7 @@ describe 'database' do
       "db > ",
     ])
   end 
+  
   it 'allows printing out the structure of a one-node btree' do
     script = [3, 1, 2].map do |i|
       "insert #{i} user#{i} person#{i}@example"
@@ -132,9 +134,25 @@ describe 'database' do
       "db > Executed.",
       "db > Tree:",
       "- leaf (size 3)",
-      "  - 0 : 3",
-      "  - 1 : 1",
-      "  - 2 : 2",
+      "  - 0 : 1",
+      "  - 1 : 2",
+      "  - 2 : 3",
+      "db > ",
+    ])
+  end
+
+  it 'prints an error message if there is a duplicate id' do
+    script = [
+      "insert 1 user1 person1@example.com",
+      "insert 1 user1 person1@example.com",
+      "select",
+      ".exit",
+    ]
+    result = run_script(script)
+    expect(result).to match_array([
+      "db > Executed.",
+      "db > Error: Duplicate key.",
+      "db > (1, user1, person1@example.com)",
       "db > ",
     ])
   end
