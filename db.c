7941939253a8b5788ea3841f7bc00b614c4bf21d
lng2020 <nanguanlin6@gmail.com>
2023-07-03 23:38:01 +0800

    add log info

--- db.c
+++ db.c
@@ -124,6 +124,15 @@ void print_row(Row* row){
     printf("(%d, %s, %s)\n", row->id, row->username, row->email);
 }
 
+void print_constants(){
+    printf("ROW_SIZE: %d\n", ROW_SIZE);
+    printf("COMMON_NODE_HEADER_SIZE: %d\n", COMMON_NODE_HEADER_SIZE);
+    printf("LEAF_NODE_HEADER_SIZE: %d\n", LEAF_NODE_HEADER_SIZE);
+    printf("LEAF_NODE_CELL_SIZE: %d\n", LEAF_NODE_CELL_SIZE);
+    printf("LEAF_NODE_SPACE_FOR_CELLS: %d\n", LEAF_NODE_SPACE_FOR_CELLS);
+    printf("LEAF_NODE_MAX_CELLS: %d\n", LEAF_NODE_MAX_CELLS);
+}
+
 u_int32_t* leaf_node_num_cells(void* node){
     return node + LEAF_NODE_NUM_CELLS_OFFSET;
 }
@@ -358,6 +367,10 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer, Table* table){
     if (strcmp(input_buffer->buffer, ".exit") == 0){
         db_close(table);
         exit(EXIT_SUCCESS);
+    }else if(strcmp(input_buffer->buffer, ".btree") == 0){
+        printf("Constants:\n");
+        print_constants();
+        return META_COMMAND_SUCCESS;
     }else {
         return META_COMMAND_UNRECOGNIZED_COMMAND;
     }
