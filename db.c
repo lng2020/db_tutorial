bb66abc7d8f67a9afa9ee2b06c16aa1a76673b86
lng2020 <nanguanlin6@gmail.com>
2023-07-05 01:14:36 +0800

    recursively searching tree

--- db.c
+++ db.c
@@ -500,6 +500,33 @@ Cursor* leaf_node_find(Table* table, u_int32_t page_num, u_int32_t key){
     return cursor;
 }
 
+Cursor* internal_node_find(Table* table, u_int32_t page_num, u_int32_t key){
+    void* node = get_page(table->pager, page_num);
+    u_int32_t num_keys = *internal_node_num_keys(node);
+
+    u_int32_t min_index = 0;
+    u_int32_t max_index = num_keys;
+
+    while(min_index != max_index){
+        u_int32_t index = (min_index + max_index) / 2;
+        u_int32_t key_to_right = *internal_node_key(node, index);
+        if(key_to_right >= key){
+            max_index = index;
+        }else{
+            min_index = index + 1;
+        }
+    }
+
+    u_int32_t child_num = *internal_node_child(node, min_index);
+    void* child = get_page(table->pager, child_num);
+    switch(get_node_type(child)){
+        case NODE_LEAF:
+            return leaf_node_find(table, child_num, key);
+        case NODE_INTERNAL:
+            return internal_node_find(table, child_num, key);
+    }
+}
+
 Cursor* table_start(Table* table){
 	Cursor* cursor = malloc(sizeof(Cursor));
 	cursor->table = table;
@@ -520,8 +547,7 @@ Cursor* table_find(Table* table, u_int32_t key) {
     if (get_node_type(root_node) == NODE_LEAF) {
         return leaf_node_find(table, root_page_num, key);
     } else {
-        printf("Need to implement searching an internal node.\n");
-        exit(EXIT_FAILURE);
+        return internal_node_find(table, root_page_num, key);
     }
 }
 
